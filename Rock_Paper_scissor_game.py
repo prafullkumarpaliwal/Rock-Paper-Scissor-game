2adda41ddad94b408002b0e0ff1492dfd0f00203
prafullkumarpaliwal <54141458+prafullkumarpaliwal@users.noreply.github.com>
2020-01-14 03:00:57 +0530

    Update_version_1.1_game
    here we try to reduce the complexity of the project

--- Rock_Paper_scissor_game.py
+++ Rock_Paper_scissor_game.py
@@ -1,37 +1,29 @@
 import random
 def main_function():
     user_input = input("Choose any option (rock,paper,scissor):")
-    if user_input.lower() == "rock" or user_input.lower()=="paper" or user_input.lower()=="scissor":
-
-        store_value = ["rock", "paper", "scissor"]
-        diplay_value = random.choice(store_value)
-        print(f"oppnent select: {diplay_value}")
-
-        if user_input == diplay_value:
-            print("No result play again")
-        elif user_input != diplay_value:
-            if user_input.lower() == "rock" and diplay_value == "paper":
+    new_input=user_input.lower()
+    store_value = ["rock", "paper", "scissor"]
+    diplay_value = random.choice(store_value)
+    print(f"oppnent select: {diplay_value}")
+    if new_input==diplay_value:
+        print("No result play again")
+    elif new_input == "rock":
+         if diplay_value=="paper":
                 print("You loss")
-            elif user_input.lower() == "rock" and diplay_value == "scissor":
-                print("You win")
-
-
-
-            elif user_input.lower() == "paper" and diplay_value == "rock":
-                print("You win")
-            elif user_input.lower() == "paper" and diplay_value == "scissor":
-                print("You loss")
-
-
-
-            elif user_input.lower() == "scissor" and diplay_value == "paper":
-                print("You win")
-            elif user_input.lower() == "scissor" and diplay_value == "rock":
-                print("You loss")
+         else:
+                print("you win")
+    elif new_input=="paper":
+        if diplay_value=="rock":
+            print("you win")
+        else:
+            print("you loss")
+    elif new_input=="scissor":
+       if diplay_value=="rock":
+           print("you loss")
+       else:
+           print("you win")
     else:
         print("Wrong input" )
-
-
 main_function()
 while True:
  play_again= input("Do you wanna play again(y/n): ")
